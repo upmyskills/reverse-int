27f26ce32f9ba6d9927fb3fd6dcde3319374cfbc
KotAtWork <RadioDeadInside>
2021-03-02 09:23:17 +0300

    feat: create logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function reverse (n) {
-  
+    const res = n.toString().split('').reverse();
+    return parseInt(res.join(''));
 }
+
+
+
+// function reverse (n) {
+//     const res = n.toString().split('').reverse();
+//     return parseInt(res.join(''));
+// }
+
+
+
+// reverse(-252);
+// reverse(0);
+// reverse(228);
